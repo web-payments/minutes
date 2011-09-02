77b43736f344b2055be02979d3a169d0f1d33773
Manu Sporny <msporny@digitalbazaar.com>
2011-09-02 14:59:20 -0400

    Added audio for 2011-09-02 telecon and updated scrawl.js w/ new names.

--- scribe-tool/scrawl.js
+++ scribe-tool/scrawl.js
@@ -10,76 +10,38 @@
 
    var people = 
    {
-      "Bradley Allen":
-      {
-         "alias": ["ballen"],
-         "homepage": "http://bradleypallen.org/about"
-      },
-      "Nicolas Dufour":
-      {
-         "alias": ["capnemo"],
-         "homepage": "http://www.nemoworld.info/"
-      },
-      "Danny Ayers":
-      {
-         "alias": ["danja"],
-         "homepage": "http://dannyayers.com/"
-      },
-      "Dave Lehn":
-      {
-         "alias": ["taaz"],
-         "homepage": "http://dil.lehn.org/"
-      },
       "Michael Johnson":
       {
-         "alias": ["mjohnson"]
-      },
-      "Gregg Kellogg":
-      {
-         "alias": "gkellogg",
-         "homepage": "http://greggkellogg.net/pages/about"
-      },
-      "Markus Lanthaler":
-      {
-         "alias": ["MarkusLanthaler"],
-         "homepage": "https://twitter.com/markuslanthaler"
-      },
-      "Niklas Lindström":
-      {
-         "alias": ["lindstream"]
+         "alias": ["mjohnson", "mjohnson_db"]
       },
       "David I. Lehn":
       {
-         "alias": ["dil", "dlehn"],
+         "alias": ["taaz", "dil", "dlehn"],
          "homepage": "http://dil.lehn.org/"
       },
       "Dave Longley":
       {
          "alias": ["dlongley", "dlongley-db"]
       },
-      "Alfonso Martin":
-      {
-         "alias": ["amartin"]
-      },
-      "Alexandre Passant":
-      {
-         "alias": ["terraces"],
-         "homepage": "http://apassant.net/"
-      },
-      "Nathan Rixham":
-      {
-         "alias": ["webr3"],
-         "homepage": "http://webr3.org/"
-      },
       "Manu Sporny":
       {
          "alias": ["manu-db", "manu1", "manu`"],
          "homepage": "http://manu.sporny.org/about"
       },
-      "Thomas Steiner":
+      "Pelle Braendgaard":
       {
-         "alias": ["tomayac"],
-         "homepage": "https://plus.google.com/103733874714446059779/about"
+         "alias": ["pelleb"],
+         "homepage": "http://stakeventures.com/pages/whoami"
+      },
+      "Jeff Sayre":
+      {
+         "alias": ["jeffsayre"],
+         "homepage": "http://jeffsayre.com/"
+      },
+      "Dmitry Gorilovsky":
+      {
+         "alias": ["mitgor"],
+         "homepage": "http://aintsys.com/en/index.html"
       },
       "Ted Thibodeau Jr.":
       {
