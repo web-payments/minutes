03998b6b366840f38ad2ee42382b4cbaf24af633
Manu Sporny <msporny@digitalbazaar.com>
2013-12-29 21:22:24 -0500

    Added ability to contact WordPress blog to publish minutes.

--- scribe-tool/index.js
+++ scribe-tool/index.js
@@ -4,10 +4,17 @@ var fs = require('fs')
 var path = require('path');
 var program = require('commander');
 var scrawl = require('./scrawl');
+var wp = require('wporg');
 
 program
   .version('0.3.0')
   .option('-d, --directory <directory>', 'The directory to process.')
+  .option('-h, --html', 'If set, write the minutes to an index.html file')
+  .option('-w, --wordpress', 'If set, publish the minutes to the blog')
+  .option('-e, --email', 'If set, publish the minutes to the mailing list')
+  .option('-t, --twitter', 'If set, publish the minutes to Twitter')
+  .option('-g, --google', 'If set, publish the minutes to G+')
+  .option('-q, --quiet', 'Don\'t print status information to the console')
   .parse(process.argv);
 
 if(!program.directory) {
@@ -50,14 +57,61 @@ async.waterfall([ function(callback) {
   // generate the index.html file
   var minutes = 
     htmlHeader + 
-    '<section><div class="container"><div class="row white"><br><div class="col-lg-offset-2 col-lg-8">' +
+    '<section><div class="container"><div class="row white"><br>' +
+    '<div class="col-lg-offset-2 col-lg-8">' +
     scrawl.generateMinutes(logData, 'html') + '</div></div></div></section>' + 
     htmlFooter;
   callback(null, minutes);
 }, function(minutes, callback) {
   // write the index.html file to disk
-  console.log(minutes);
-  callback();
+  if(program.html) {
+    if(!program.quiet) {
+      console.log('scrawl: Writing', indexFile);
+    }
+    fs.writeFile(indexFile, minutes, {}, callback);
+  } else {
+    callback();
+  }
+}, function(callback) {
+  if(program.wordpress) {
+    var prompt = require('prompt');
+    prompt.start();
+    prompt.get({
+      properties: {
+        username: {
+          description: 'Enter the Web Payments WordPress username',
+          pattern: /^.{4,}$/,
+          message: 'The username must be at least 4 characters.',
+          'default': 'msporny'
+        },
+        password: {
+          description: 'Enter the user\'s password',
+          pattern: /^.{4,}$/,
+          message: 'The password must be at least 4 characters.',
+          hidden: true,
+          'default': 'password'
+        }
+      }
+    }, function(err, results) {
+      var client = wp.createClient({
+          username: results.username,
+          password: results.password,
+          url: 'https://www.w3.org/community/webpayments/xmlrpc.php'
+      });
+      client.getUsersBlogs(function(err, data) {
+        if(err){
+          console.log(err);
+        }
+        else {
+          console.log(data);
+          // Do something.
+        }
+        callback();
+      });      
+    });
+  } else {
+    callback();
+  }
 }], function(err) {
   // check to ensure there were no errors
   if(err) {
